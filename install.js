2428c29f06a9e8910f33be0350839cc69cbf025f
DarkRiDDeR <darkridder.pro@gmail.com>
2024-03-21 21:51:07 +0800

    update qubic data and update sql table

--- install.js
+++ install.js
@@ -1,47 +1,59 @@
-import { getPasswordHash, users, logger, db } from './config.js'
+import { install, users } from './config.js'
+import { connectDB, getTimestampOfLastWednesday, getPasswordHash } from './functions.js'
 
 
-
+const db = connectDB()
 db.serialize(() => {
+    db.run(`CREATE TABLE config (
+        name VARCHAR NOT NULL,
+        value TEXT
+    );`)
     db.run(`CREATE TABLE users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        login VARCHAR(20) NOT NULL UNIQUE,
+        login VARCHAR(25) NOT NULL UNIQUE,
         password VARCHAR(64) NOT NULL,
         wallet VARCHAR(60) NOT NULL
-        --number_workers INTEGER NOT NULL
-    );`);
+    );`)
     db.run(` CREATE TABLE workers (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        -- worker name "login.name"
-        name VARCHAR(30) NOT NULL UNIQUE,
-        user_id INTEGER,
+        name VARCHAR(25) NOT NULL,
+        user_id INTEGER NOT NULL,
+        UNIQUE(name, user_id),
         FOREIGN KEY (user_id)  REFERENCES users (id)
-    );`);
+    );`)
     db.run(`CREATE TABLE epoch_statistics (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         epoch INTEGER,
+        datetime INTEGER NOT NULL,
         user_id INTEGER,
         sol INTEGER,
         its REAL,
+        share REAL,
         FOREIGN KEY (user_id) REFERENCES users (id)
-    );`);
+    );`)
+    // previous epoch is 0
+    db.run(
+        `INSERT INTO epoch_statistics(epoch, datetime, user_id, sol, its, share)  VALUES (?,?,1,0,0,0);`,
+        [install.currentEpoch-1, getTimestampOfLastWednesday() - 7*24*60*60]
+    )
     db.run(`CREATE TABLE workers_statistics (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        worker VARCHAR(30) NOT NULL,
+        user_id VARCHAR(25) NOT NULL,
+        worker_id VARCHAR(25) NOT NULL,
         last_active VARCHAR(25),
-        date_time INTEGER NOT NULL,
         its INTEGER NOT NULL,
         sol INTEGER NOT NULL,
-        is_active BOOLEAN NOT NULL
-        --FOREIGN KEY (user_id) REFERENCES users (id),
-        --FOREIGN KEY (worker_id) REFERENCES workers (id)
-    );`);
+        is_active BOOLEAN NOT NULL,
+        datetime INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users (id),
+        FOREIGN KEY (worker_id) REFERENCES workers (id)
+    );`)
 
     const stmt = db.prepare("INSERT INTO users(login, password, wallet) VALUES (?,?,?)")
     for(let user of users) {
         stmt.run(user[0], getPasswordHash(user[1]), user[2])
     }
     stmt.finalize()
-});
+})
 
-db.close();
+db.close()
--- qubicLiUpdater.js
+++ qubicLiUpdater.js
@@ -1,82 +1,101 @@
-//import https from 'node:https'
-import { logger, qubicLiUser, db } from './config.js'
-
+import { qubic } from './config.js'
+import { logger, connectDB, getPasswordHash } from './functions.js'
 
 const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0'
 const timeout = 30000
-
+let serverData = ''
 let response, result, token
+
 try {
-    let postData = JSON.stringify({ 'userName': qubicLiUser.login, 'password': qubicLiUser.password, 'twoFactorCode': '' })
-    response  = await fetch('https://api.qubic.li/Auth/Login', {
-        method: 'POST',
-        body: postData,
-        headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': Buffer.byteLength(postData),
-          'User-Agent': userAgent
-        },
-        timeout: timeout
-    })
-
-    result = await response.json()
-    token = result.token
-
-    if (!token) {
-        logger.error(result, 'Error token')
-    } else {
-
-        /*
-        miners: [
-            {
-            id: '1dfff512-a17a-4e6a-8489-2e3640303bb4',
-            minerBinaryId: null,
-            alias: 'admin.2680',
-            version: [Object],
-            outdatedVersion: false,
-            lastActive: '2024-03-18T09:05:59.37',
-            currentIts: 132,
-            currentIdentity: 'OZLVNWQIXWVDYBSRZNIFARWFJDXAUTJSSWRXLWSQYEIGOXVZSSYTILNAXHQJ',
-            solutionsFound: 0,
-            threads: null,
-            totalFeeTime: 0,
-            feeReports: [],
-            isActive: true
-            }
-        ],
-        */
-        response  = await fetch('https://api.qubic.li/My/MinerControl', {
-            method: 'GET',
+    if (qubic.specificDataServer) {
+        response  = await fetch(qubic.specificDataServer, {
             headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
                 'User-Agent': userAgent
             },
-            timeout: timeout,
+            timeout: timeout
         })
-        result = await response.json()
-        //console.log(result)
+        serverData = await response.json()
+    } else {
+        let postData = JSON.stringify({ 'userName': qubic.login, 'password': qubic.password, 'twoFactorCode': '' })
+        response  = await fetch('https://api.qubic.li/Auth/Login', {
+            method: 'POST',
+            body: postData,
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(postData),
+                'User-Agent': userAgent
+            },
+            timeout: timeout
+        })
 
-        if (result.miners) {
-            let dbUsers = new Map() // [login, id]
-            let dbWorkers = new Map() // [name, {id, user_id}]
-            await new Promise((resolve, reject) => {
-                db.serialize(() => {
-                    db.each("SELECT name, id, user_id FROM workers", (err, row) => {
-                        if (err) return reject(err)
-                        dbWorkers.set(row.name, {id: row.id, userId: row.user_id})
-                        resolve()
-                    })
-                    db.get("", function(err, row)  {
-                        resolve(true)
-                    }) 
-                })
+        result = await response.json()
+        token = result.token
+
+        if (!token) {
+            logger.error(result, 'Error token')
+        } else {
+
+            /*
+            miners: [
+                {
+                id: '1dfff512-a17a-4e6a-8489-2e3640303bb4',
+                minerBinaryId: null,
+                alias: 'admin.2680',
+                version: [Object],
+                outdatedVersion: false,
+                lastActive: '2024-03-18T09:05:59.37',
+                currentIts: 132,
+                currentIdentity: 'OZLVNWQIXWVDYBSRZNIFARWFJDXAUTJSSWRXLWSQYEIGOXVZSSYTILNAXHQJ',
+                solutionsFound: 0,
+                threads: null,
+                totalFeeTime: 0,
+                feeReports: [],
+                isActive: true
+                }
+            ],
+            */
+            response  = await fetch('https://api.qubic.li/My/MinerControl', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                    'User-Agent': userAgent
+                },
+                timeout: timeout,
             })
-            await new Promise((resolve, reject) => {
+            serverData = await response.json()
+            serverData = serverData.miners
+        }
+    }
+
+
+
+
+    if (serverData) {
+        const db = connectDB()
+        let dbUsers = new Map() // [login, id]
+        let dbWorkers = new Map() // [user_id.name, id]
+        const execGetDbUsers = () => {
+            dbUsers = new Map()
+            return new Promise((resolve, reject) => {
                 db.serialize(() => {
                     db.each("SELECT id, login FROM users", (err, row) => {
                         if (err) return reject(err)
                         dbUsers.set(row.login, row.id)
+                    })
+                    db.get("", function(err, row)  {
+                        resolve(true)
+                    }) 
+                })
+            })
+        }
+        const execGetDbWorkers = () => {
+            dbWorkers = new Map()
+            return new Promise((resolve, reject) => {
+                db.serialize(() => {
+                    db.each("SELECT name, id, user_id FROM workers", (err, row) => {
+                        if (err) return reject(err)
+                        dbWorkers.set(row.user_id + '.' + row.name, row.id)
                         resolve()
                     })
                     db.get("", function(err, row)  {
@@ -84,82 +103,107 @@ try {
                     }) 
                 })
             })
+        }
+        await execGetDbUsers()
 
+        let stats = new Map() // [user.worker, [user, worker, its, sol, lastActive, isActive]]
+        let poolUsers = new Set() 
+        let poolWorkers = new Map() // [user.worker, [user, worker]]
+        for(let item of serverData) {
+            let alias = item.alias.trim().toLowerCase().split('.', 2)
+            let worker = alias[0].trim()
+            let user = 'none' // no detect user
+            if (alias.length > 1) {
+                user = worker
+                worker  = alias[1].trim()
+            }
+            const userWorker = user + '.' + worker
+            const its = parseInt(item.currentIts) 
+            const sol = parseInt(item.solutionsFound)
+            const lastActive = item.lastActive
+            const isActive = item.isActive ? 1 : 0
 
-            let stats = new Map() // [user.worker, [user, worker, its, sol, lastActive, isActive]]
-            let newWorkers = [] // [user.worker]
-            for(let item of result.miners) {
-                const alias = item.alias.trim().toLowerCase().split('.', 2)
-                if (alias[0] && alias[1]) {
-                    const id = item.id
-                    const user = alias[0].trim()
-                    const worker  = alias[1].trim()
-                    const workerName= user + '.' + worker
-                    const its = parseInt(item.currentIts) 
-                    const sol = parseInt(item.solutionsFound)
-                    const lastActive = item.lastActive
-                    const isActive = item.isActive ? 1 : 0
+            poolUsers.add(user)
+            poolWorkers.set(userWorker, [user, worker])
 
-                    if (dbUsers.has(user)) {
-                        if (newWorkers.indexOf(workerName) == -1 && !dbWorkers.has(workerName)) {
-                            newWorkers.push(workerName)
-                        }
-                        if (stats.has(workerName)) { // repeat user.worker
-                            if (isActive) {
-                                let statItem = stats.get(workerName)
-                                statItem.sol += sol
-                                statItem.its += its
-                                stats.set(workerName, statItem)
-                            }
-                        } else {
-                            stats.set(workerName, { its: its, sol: sol, lastActive: lastActive, isActive: isActive})
-                        }
-                    } else {
-                        logger.warn(item, 'Worker and such user do not exist')
-                    }
-                } else {
-                    logger.warn(item, 'Incorrect alias')
+            if (stats.has(userWorker)) { // repeat user.worker
+                if (isActive) {
+                    let statItem = stats.get(userWorker)
+                    statItem.sol += sol
+                    statItem.its += its
+                    stats.set(userWorker, statItem)
                 }
+            } else {
+                stats.set(userWorker, { user, worker, its, sol, lastActive, isActive})
             }
+        }
+        let newUsers = [...dbUsers.keys()]
+        newUsers = [...poolUsers].filter(i => !newUsers.includes(i))
 
-            if (newWorkers) {
-                await new Promise((resolve, reject) => {
-                    db.serialize(() => {
-                        // new workers
-                        const stmt = db.prepare("INSERT INTO workers(name, user_id) VALUES (?,?)")
-                        for(let worker of newWorkers) {
-                            stmt.run(worker, dbUsers.get(worker.split('.')[0]))
-                        }
-                        stmt.finalize(err => { if (err) return reject(err) })
-                        resolve(true)
-                    })
-                })
-            }
-
+        if (newUsers) {
             await new Promise((resolve, reject) => {
                 db.serialize(() => {
-                    const datetime = parseInt(Date.now() / 1000) // Unix time
-                    const stmt = db.prepare("INSERT INTO workers_statistics(worker, last_active, date_time, its, sol, is_active) VALUES (?,?,?,?,?,?)")
-                    stats.forEach((item, key) => {
-                        stmt.run(key, item.lastActive, datetime, item.its, item.sol, item.isActive)
-                    });
-                    stmt.finalize(err => { if (err) return reject(err) })
+                    const stmt = db.prepare("INSERT INTO users (login, password, wallet) VALUES (?,?,?)")
+                    let lastUser = ''
+                    for(let user of newUsers) {
+                        lastUser = user
+                        stmt.run(user, getPasswordHash(user), '')
+                    }
+                    stmt.finalize(err => { if (err) return reject([err, lastUser]) })
                     resolve(true)
                 })
             })
+            await execGetDbUsers()
+        }
 
-            /*console.log(dbUsers)
-            console.log(dbWorkers)
-            console.log(newWorkers)
-            console.log(stats)*/
-        } else {
-            logger.error(result, 'Error no miners')
-        } 
-    }
+        // workers
+        await execGetDbWorkers()
+        let newWorkers = [...poolWorkers.values()].filter(item => {
+            return !dbWorkers.has(dbUsers.get(item[0]) + '.' + item[1])
+        })
+        //newWorkers.push(['balakin', '2680dual'])
+        if (newWorkers) {
+            await new Promise((resolve, reject) => {
+                db.serialize(() => {
+                    const stmt = db.prepare("INSERT INTO workers(user_id, name) VALUES (?,?);")
+                    let lastWorker = ''
+                    for(let worker of newWorkers) {
+                        lastWorker = worker
+                        stmt.run(dbUsers.get(worker[0]), worker[1])
+                    }
+                    stmt.finalize(err => { if (err) return reject([err, lastWorker]) })
+                    resolve(true)
+                })
+            })
+        }
 
+        await new Promise((resolve, reject) => {
+            db.serialize(() => {
+                const datetime = parseInt(Date.now() / 1000) // Unix time
+                const stmt = db.prepare("INSERT INTO workers_statistics(user_id, worker_id, last_active, datetime, its, sol, is_active) VALUES (?,?,?,?,?,?,?)")
+                stats.forEach(item => {
+                    const userId = dbUsers.get(item.user)
+                    const workerId = dbWorkers.get(userId + '.' + item.worker)
+                    stmt.run(userId, workerId, item.lastActive, datetime, item.its, item.sol, item.isActive)
+                });
+                stmt.finalize(err => { if (err) return reject(err) })
+                resolve(true)
+            })
+        })
+
+        db.close()
+
+        /*console.log({dbUsers})
+        console.log({dbWorkers})
+        console.log({newUsers})
+        console.log({newWorkers})
+        //console.log({poolWorkers})
+        console.log(stats)*/
+        
+    } else {
+        logger.error(serverData, 'Error: miners data')
+    } 
 } catch (err) {
     logger.error(err);
 }
 
-
-db.close()
